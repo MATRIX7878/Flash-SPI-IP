bface1c1ab456759a942aff6fb641316362ef89c
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2025-01-30 16:08:36 -0800

    Update memory.tcl

--- memory.tcl
+++ memory.tcl
@@ -1,20 +1,24 @@
 if {[file exists rtl_work]} {
 	vdel -lib rtl_work -all
 }
+
 vlib rtl_work
+vlib flashStates
 
 vmap work rtl_work
+vmap flashStates rtl_work
 
-vcom -work work [pwd]/*.vhd
+vcom -work flashStates [pwd]/flashStates.vhd
+vcom -work work [pwd]/toplevel.vhd -suppress 1339
+vcom -work work [pwd]/flash.vhd
+vcom -work work [pwd]/UART.vhd
 
 vsim toplevel
 
 add wave -recursive *
 
-force clk 0, 1 13.5 -r 27
-force RST 1 0, 0 50
-force btn1 1 0
-force btn2 1 0
+force clk 0, 1 18.5 -r 37
+force reset 1 0, 0 50
 
 view structure
 view signals
