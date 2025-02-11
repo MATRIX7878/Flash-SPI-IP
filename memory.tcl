a5d1edf844b0f323adc573f888184425e7721c06
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2025-02-10 18:09:59 -0800

    Update memory.tcl

--- memory.tcl
+++ memory.tcl
@@ -9,21 +9,23 @@ vmap work rtl_work
 vmap flashStates rtl_work
 
 vcom -work flashStates [pwd]/flashStates.vhd
-vcom -work work [pwd]/toplevel.vhd -suppress 1339
 vcom -work work [pwd]/flash.vhd
-vcom -work work [pwd]/UART.vhd
+vcom -work work [pwd]/UARTTX.vhd
+vcom -work work [pwd]/conv.vhd
+vcom -work work [pwd]/top.vhd
 
-vsim toplevel
+vsim top
 
 add wave -recursive *
 
 force clk 0, 1 18.5 -r 37
 force reset 1 0, 0 50
+force MISO 'h0B15 1 ms
 
 view structure
 view signals
 
-run 250 us
+run 5 ms
 
 view -undock wave
 wave zoomfull
