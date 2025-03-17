0dea8644f9968d9ca6bec40e0b42e2e53a9fafe4
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2025-03-16 23:27:57 -0700

    Update conv.vhd

--- conv.vhd
+++ conv.vhd
@@ -3,31 +3,32 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD_UNSIGNED.ALL;
 
 ENTITY conv IS
-    PORT(clk : IN STD_LOGIC;
-         char : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-         hexLow, hexHigh : OUT STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0')
-        );
+    PORT (clk : IN STD_LOGIC;
+          char : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+          hexHigh, hexLow : OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
+         );
 END ENTITY;
 
 ARCHITECTURE behavior OF conv IS
-SIGNAL flip : STD_LOGIC_VECTOR (7 DOWNTO 0) := (OTHERS => '0');
-
-IMPURE FUNCTION FLIPPER (data : STD_LOGIC_VECTOR) RETURN STD_LOGIC_VECTOR IS
-VARIABLE var : STD_LOGIC_VECTOR (7 DOWNTO 0) := (OTHERS => '0');
-BEGIN
-    FOR i IN 0 TO 7 LOOP
-        var(i) := data(7 - i);
-    END LOOP;
-    RETURN var;
-END FUNCTION;
+SIGNAL newHigh, newLow, upper, lower : STD_LOGIC_VECTOR (7 DOWNTO 0) := (OTHERS => '0');
 
 BEGIN
+    hexHigh <= upper;
+    hexLow <= lower;
+
     PROCESS(ALL)
     BEGIN
+        newHigh <= upper;
+        newLow <= lower;
+        newHigh <= char(7 DOWNTO 4) + TO_STDLOGICVECTOR(48, 8) WHEN char(7 DOWNTO 4) <= 9 ELSE char(7 DOWNTO 4) + TO_STDLOGICVECTOR(55, 8);
+        newLow <= char(3 DOWNTO 0) + TO_STDLOGICVECTOR(48, 8) WHEN char(3 DOWNTO 0) <= 9 ELSE char(3 DOWNTO 0) + TO_STDLOGICVECTOR(55, 8);
+    END PROCESS;
+
+    PROCESS (ALL)
+    BEGIN
         IF RISING_EDGE(clk) THEN
-            flip <= FLIPPER(char);
-            hexLow <= flip(7 DOWNTO 4) + TO_STDLOGICVECTOR(48, 8) WHEN flip(7 DOWNTO 4) <= 9 ELSE flip(7 DOWNTO 4) + TO_STDLOGICVECTOR(55, 8);
-            hexHigh <= flip(3 DOWNTO 0) + TO_STDLOGICVECTOR(48, 8) WHEN flip(3 DOWNTO 0) <= 9 ELSE flip(3 DOWNTO 0) + TO_STDLOGICVECTOR(55, 8);
+            upper <= newHigh;
+            lower <= newLow;
         END IF;
     END PROCESS;
 END ARCHITECTURE;
